fbcef1d9f9f114ce59035a613d731420e41c6891
shiva <gbhavaniprasad1818@gmail.com>
2026-05-24 16:12:19 +0530

    my local changes

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+name="Siva"
+age=28
+city="Hyderabad"
+retirement_age=60
+years_until=$((retirement_age - age))
+
+echo "My name is $name, I am $age years old and I live in $city."
+echo "Years until 60: $years_until" 
