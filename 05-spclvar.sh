104d2aaeebfa8b60884bd2d1e9c3d53a652be20b
shiva <gbhavaniprasad1818@gmail.com>
2026-05-24 17:29:32 +0530

    my local changes

---
+++ 05-spclvar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+echo "who is running this: $USER"
+echo "which directory are we in: $(pwd)"
+echo "home directory: $HOME"
+echo "PID of the current script: $$"
+echo "Background PID: $!"
+
+echo "Background PID: $!"
+
+echo "Random number: $RANDOM"
+echo "Exit status of last command: 0"
