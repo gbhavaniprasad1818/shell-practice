b78b0f1e854687b7cd8094488d5067366d7a195c
shiva <gbhavaniprasad1818@gmail.com>
2026-05-24 17:45:10 +0530

    my local changes

--- 06-exitcodes.sh
+++ 06-exitcodes.sh
@@ -1,39 +1,14 @@
 #!/bin/bash
 
-all_success=true
-
-# 1. Run ls /tmp
-ls /tmp > /dev/null 2>&1
 if [ $? -eq 0 ]; then
-    echo "SUCCESS"
+    echo "ls /tmp executed successfully."
 else
-    echo "FAILURE"
-    all_success=false
-fi
+    echo "ls /tmp failed to execute."
 
-# 2. Run ls /fakedir
-ls /fakedir > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    echo "SUCCESS"
+if [ $? -ne 0 ]; then 
+    echo "ls /tmp is FAILED"
+exit 1
 else
-    echo "FAILURE"
-    all_success=false
+    echo "ls /tmp is SUCCESS"
 fi
-
-# 3. Ping google.com
-ping -c1 google.com > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    echo "Host reachable"
-else
-    echo "Host not reachable"
-    all_success=false
-fi
-
-# 4. Exit with appropriate code
-if [ "$all_success" = true ]; then
-    echo "All commands succeeded"
-    exit 0
-else
-    echo "One or more commands failed"
-    exit 1
 fi
