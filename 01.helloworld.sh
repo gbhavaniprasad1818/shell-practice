9e083a59c2b10a7047cf994eace9a6f166181dae
shiva <gbhavaniprasad1818@gmail.com>
2026-05-24 16:01:11 +0530

    my local changes

--- 01.helloworld.sh
+++ 01.helloworld.sh
@@ -2,3 +2,11 @@
 
 # This is a simple script to print "Hello, World!" to the console.
 echo "Hello, World!"
+
+echo "who is running this: $USER"
+
+echo "Today is: $(date)"
+
+echo "Current working directory: $(pwd)"
+
+echo" who is running this: $USER"
