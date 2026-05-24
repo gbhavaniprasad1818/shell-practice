6f9eb4e4eb24b93e3c6997c5cf28c2a1ca7326e8
shiva <gbhavaniprasad1818@gmail.com>
2026-05-24 16:33:48 +0530

    my local changes

--- 04-args.sh
+++ 04-args.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-echo "User $1 is from $2"
+PERSON1="Siva"
+PERSON2="INDIA"
+echo "User $PERSON1 is from $PERSON2"
 echo "Number of arguments: $#"
 echo "All arguments: $*"
 echo "Script name: $0"
+
+# sh 04-args.sh siva india -> User siva is from india
+# Number of arguments: 2
+# All arguments: siva india
+# Script name: 04-args.sh
+
