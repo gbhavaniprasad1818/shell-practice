deb64c8626dc1e6045e8b57d644f962713ba5150
shiva <gbhvaniprasad1818@gmail.com>
2026-05-30 18:21:01 +0530

    ram status updated

--- ram-monitor.sh
+++ ram-monitor.sh
@@ -2,6 +2,7 @@
 
 FREE_SPACE=$(free -mt | grep "Total" | awk '{print $4}')
 THRESHOLD=500
+
 echo "Free Memory: $FREE_SPACE MB"
 
 if [[ $FREE_SPACE -lt $TH ]]
@@ -9,4 +10,10 @@ then
 	echo "WARNING, RAM is running low"
 else
 	echo "RAM Space is sufficient - $FREE_SPACE M"
+fi
+
+if [[ $FREE_SPACE -lt $TH ]]; then
+    echo "Warning: Free memory is low"
+else
+    echo "Free memory is sufficient - $FREE_SPACE MB."
 fi
