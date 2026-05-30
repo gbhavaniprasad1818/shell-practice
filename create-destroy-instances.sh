dd8eec3c04d6b393d91de2187508a8d9cf4b1f21
shiva <gbhvaniprasad1818@gmail.com>
2026-05-30 18:32:22 +0530

    ram status updated

--- create-destroy-instances.sh
+++ create-destroy-instances.sh
@@ -34,7 +34,7 @@ get_instance_id(){
 for instance in $@
 do
     INSTANCE_ID=$(get_instance_id $instance)
-    if [ $ACTION == "create" ]; then
+    if [ "$ACTION" == "create" ]; then
         if [ $INSTANCE_ID == "None" ]; then
             echo "Launching Instance: roboshop-$instance"
             INSTANCE_ID=$( aws ec2 run-instances \
