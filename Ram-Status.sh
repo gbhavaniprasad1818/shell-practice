f1f686ce4feab013b4b210941205e36926795f8c
shiva <gbhvaniprasad1818@gmail.com>
2026-05-30 18:02:24 +0530

    ram status updated

--- Ram-Status.sh
+++ Ram-Status.sh
@@ -3,7 +3,7 @@
 FREE_SPACE=$(free -mt | grep "Total" | awk '{print $4}')
 echo "Free Memory: $FREE_SPACE MB"
 
-if [ [ $FREE_SPACE -lt $TH ] ]; then
+if [ $FREE_SPACE -lt $TH ]; then
     echo "Warning: Free memory is low"
 else
     echo "Free memory is sufficient - $FREE_SPACE MB."
